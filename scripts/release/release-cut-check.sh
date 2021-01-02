0f8ae7141c43e9a36c5190e5ece0d24d58f5b58f
Devlin Junker <devlin.junker@gmail.com>
2021-01-02 12:43:44 -0800

    wip: fix most errors/warnings in shell scripts

--- scripts/release/release-cut-check.sh
+++ scripts/release/release-cut-check.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-git branch -a | grep $RELEASE_BRANCH;
-
-if [ $? -eq 0 ]; then
+if [ "$(git branch -a | grep "$RELEASE_BRANCH";)" -eq 0 ]; then
   echo "::error::Release Branch Already Exists";
   exit 1        
 fi;
