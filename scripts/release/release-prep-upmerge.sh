cd09f52fa48005755d348e36fdb233a726020891
Devlin Junker <devlin.junker@gmail.com>
2021-07-24 12:21:27 -0700

    fix: noticed errors with set -eu and creating releases

--- scripts/release/release-prep-upmerge.sh
+++ scripts/release/release-prep-upmerge.sh
@@ -9,7 +9,7 @@ git checkout develop;
 
 # upmerge from branch input
 git pull;
-git merge "$1";
+git merge "$1" 2>/dev/null; # ignore error we expect heres
 
 # TODO: Resolve conflicts better (maybe https://github.com/jakub-g/git-resolve-conflict)
 git reset README.md;
